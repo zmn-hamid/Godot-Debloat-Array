fa2ce0870b2d778c26420f1363b8d6b597ac48b5
hamid <zmn-hamid@proton.me>
2025-05-20 16:21:37 +0330

    v0.9.1: huge ui updates

--- README.md
+++ README.md
@@ -1,19 +1,32 @@
 # [Beta] Debloat Exported Arrays Addon for Godot
 
-![screenshot1](https://github.com/user-attachments/assets/8c57f798-2ae9-4327-8dca-bc8cbd3e684a)
+![screenshot1](https://github.com/user-attachments/assets/5a17300e-4c65-4e9c-8af3-8067644192e5)
 
-This plugin debloats the exported Arrays by removing the `Size:` and `> Resource` fields.
-
-Attributes to (kleonc)[https://github.com/kleonc] for basically
-writing the whole plugin. I've just gathered his code into a plugin.
-
-This is the beta version because the method used is a hacky one and might not work
-for the previous versions out of box. The issue can be seen in godotengine/godot#106576.
+This plugin debloats the exported Arrays by removing the `Size:` and `> Resource` fields and
+updating the UI colors for a better item hierarchy. This is a beta version because the method used to
+achieve this relies on the UI system defined for each version. So this addon might not work for other
+versions of Godot. This is a follow up of this issue: godotengine/godot#106576
 
 > Works with Godot 4.4 - other versions not tested
+
+## Why Are These Changes Made
+
+- Size and Resource fields: They are barely usable in most cases and make the hierarchy way worse
+- Grab menu: This field was emptied out (icon set to null), but you can use the empty space to move the items. This
+feature must be implemented in the header of each item instead of an icon. Hiding it was better for
+the visual hierarchy than having it.
+- Delete and drop down buttons: They have become transparent (in background) to support the main feature and help the visual hierarchy
+- Add Element button: This button was changed to be 1. visually more satisfying 2. Help with the visual hierarchy by being transparent in the normal mode. Also because of the nature of icons (you can't align them to the center I guess) I tried hiding the icon and using a "+" text instead.
+
+This way only the necessary fields are bold, and the other buttons are in fact, secondary.
 
 ## How To Use
 
 1. Install and activate the addon
 2. Reload the project: Project > Reload Current Project
 3. Now you're good to go. check the example scene to see the results.
+
+## Attribution
+
+Thanks to (kleonc)[https://github.com/kleonc], memeber of Godot who wrote the core of this addon. I added the styling
+and UI updates.
--- addons/debloat_array/debloat_array_inspector.gd
+++ addons/debloat_array/debloat_array_inspector.gd
@@ -2,30 +2,62 @@
 extends EditorInspectorPlugin
 
 func _init() -> void:
-	var editor_tree: SceneTree = EditorInterface.get_base_control().get_tree()
-	editor_tree.node_added.connect(on_editor_node_added)
+    var editor_tree: SceneTree = EditorInterface.get_base_control().get_tree()
+    editor_tree.node_added.connect(on_editor_node_added)
 
 func on_editor_node_added(node: Node) -> void:
-	if node.get_class() == "EditorPropertyArray":
-		node.child_entered_tree.connect(func(child: Node) -> void:
-			if child is PanelContainer:
-				var hide_size_label_container := func() -> void:
-					var size_label_container: Control = child.get_child(0).get_child(0)
-					size_label_container.hide()
-				hide_size_label_container.call_deferred()
-		)
+    if node.get_class() == "EditorPropertyArray":
+        node.child_entered_tree.connect(func(child: Node) -> void:
+            if child is PanelContainer:
+                var transparent_style_all: StyleBoxFlat = load("res://addons/debloat_array/styles/da_style_transparent.tres")
+                var button_style_normal: StyleBoxFlat = load("res://addons/debloat_array/styles/da_style_button_normal.tres")
+                var button_style_hover: StyleBoxFlat = load("res://addons/debloat_array/styles/da_style_button_hover.tres")
+
+                var hide_size_label_container := func() -> void:
+                    var size_label_container: Control = child.get_child(0).get_child(0)
+                    size_label_container.hide()
+                    
+                    var add_element_button: Button = child.get_child(0).get_child(-2)
+                    add_element_button.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+                    add_element_button.size_flags_vertical = Control.SIZE_SHRINK_CENTER
+                    add_element_button.alignment = HORIZONTAL_ALIGNMENT_CENTER
+                    add_element_button.text = '+'
+                    add_element_button.icon = null
+                    add_element_button.add_theme_stylebox_override("normal", button_style_normal)
+                    add_element_button.add_theme_stylebox_override("hover", button_style_hover)
+
+                    for i in range(len(child.get_child(0).get_child(1).get_children())):
+                        var hamburgur_toggle_button: Button = child.get_child(0).get_child(1).get_child(i).get_child(0)
+                        hamburgur_toggle_button.text = ''
+                        hamburgur_toggle_button.icon = null
+                        override_styleboxes(hamburgur_toggle_button, transparent_style_all)
+
+                        var delete_button: Button = child.get_child(0).get_child(1).get_child(i).get_child(-1)
+                        override_styleboxes(delete_button, transparent_style_all)
+
+                        var drop_down_button = child.get_child(0).get_child(1).get_child(i).get_child(-2).get_child(0)
+                        if len(drop_down_button.get_children()):
+                            override_styleboxes(drop_down_button.get_child(-1), transparent_style_all)
+                hide_size_label_container.call_deferred()
+        )
+
+func override_styleboxes(node: Node, style_box: StyleBox) -> void:
+    node.add_theme_stylebox_override("normal", style_box)
+    node.add_theme_stylebox_override("hover", style_box)
+    node.add_theme_stylebox_override("pressed", style_box)
+    node.add_theme_stylebox_override("focus", style_box)
 
 func _can_handle(object: Object) -> bool:
-	return true
+    return true
 
 func _parse_end(object: Object) -> void:
-	if object is Resource:
-		add_custom_control(ResourceGroupHider.new())
-		
+    if object is Resource:
+        add_custom_control(ResourceGroupHider.new())
+
 class ResourceGroupHider:
-	extends Control
-	
-	func _ready() -> void:
-		var resource_group_container: Control = get_parent().get_child(-2)
-		resource_group_container.hide()
-		queue_free()
+    extends Control
+    
+    func _ready() -> void:
+        var resource_group_container: Control = get_parent().get_child(-2)
+        resource_group_container.hide()
+        queue_free()
